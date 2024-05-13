fb5075a7280bd0502f5354352a8f51068e56159a
deni <denise.peste@gmail.com>
2024-05-13 11:58:39 +0200

    inserimento immagini

---
+++ progetto.py
@@ -0,0 +1,11 @@
+import pygame
+import random
+from sys import exit
+
+pygame.init()
+Clock= pygame.time.Clock()
+
+cielo= pygame.image.load("immagini/cielo.png")
+cornice= pygame.image.load("immagini/cornice.png")
+nuvoletta= pygame.image.load("immagini/nuvoletta.png")
+tom= pygame.image.load("immagini/tom.png")
