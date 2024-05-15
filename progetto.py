aa2b95be784afa90a1365676c35c887780638d0e
deny137 <denise.peste@gmail.com>
2024-05-15 13:35:28 +0200

    creazione finestra

--- progetto.py
+++ progetto.py
@@ -1,6 +1,6 @@
-import pygame
-import random
-from sys import exit
+import pygame, sys
+from random import randint
+from pygame.locals import *
 
 pygame.init()
 Clock= pygame.time.Clock()
@@ -9,3 +9,13 @@ cielo= pygame.image.load("immagini/cielo.png")
 cornice= pygame.image.load("immagini/cornice.png")
 nuvoletta= pygame.image.load("immagini/nuvoletta.png")
 tom= pygame.image.load("immagini/tom.png")
+
+WINDOW_SIZE= (700,500)
+SCREEN= pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Talking Tom!")
+
+while True:
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            sys.exit()
