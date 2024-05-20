7c74470cc6c91f469aa260ec7c23c1a1e70cfdef
deny137 <denise.peste@gmail.com>
2024-05-20 12:12:14 +0200

    aggiorna

--- progetto.py
+++ progetto.py
@@ -40,6 +40,9 @@ def inizializza():
     fast_tom=0
     nuvole=[]
     nuvole.append(nuvolette())
+def aggiorna():
+    pygame.display.update()
+    pygame.time.Clock().tick(FPS)
 
 def aggiorna():
     pygame.display.update()
