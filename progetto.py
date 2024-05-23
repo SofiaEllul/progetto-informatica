e8fedc45223961ae58188d18a15d7a5bbe771681
deny137 <denise.peste@gmail.com>
2024-05-23 18:56:36 +0200

    lavorando sulle nuvole e su gameover

--- progetto.py
+++ progetto.py
@@ -10,9 +10,10 @@ cornice2= pygame.image.load("immagini/cornice2.png")
 cornicesu= pygame.transform.flip(cornice2, False, True)
 nuvoletta= pygame.image.load("immagini/nuvoletta.png")
 tom= pygame.image.load("immagini/tom .png")
+game_over=pygame.image.load("immagini/gameover.png")
 
 FPS=50
-vel=3
+vel=5
 vel_nuvole=3
 
 WINDOW_SIZE= (605,500)
@@ -21,11 +22,11 @@ pygame.display.set_caption("Talking Tom!")
 
 class nuvolette:
     def __init__(self):
-        self.x=100
-        self.y=randint(-75,150)
+        self.x=700
+        self.y=randint(-75,200)
     def movimento(self):
-        self.x=vel
-        SCREEN.blit(nuvoletta, (self.x,self.y+randint(0,99)))
+        self.x-=vel
+        SCREEN.blit(nuvoletta, (self.x,self.y+100))
 
 def immagini():
     SCREEN.blit(cielo, (0,0))
@@ -50,15 +51,27 @@ def aggiorna():
     pygame.display.update()
     pygame.time.Clock().tick(FPS)
 
+def gameover():
+    SCREEN.blit(game_over, (192,60))
+    aggiorna()
+    ricomincia=False
+    while not ricomincia:
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN and event.key==pygame.K_SPACE:
+                inizializza()
+                ricomincia=True
+            if event.type==pygame.QUIT:
+                pygame.quit()
+
 inizializza()
 
 while True:
     cornicegiu_x-=vel
-    if cornicegiu_x<-608:
+    if cornicegiu_x<-300:
         cornicegiu_x=0
 
     cornicesu_x-=vel
-    if cornicesu_x<-608:
+    if cornicesu_x<-300:
         cornicesu_x=0
 
     fast_tom+=1
@@ -69,6 +82,12 @@ while True:
             fast_tom=-10
         if event.type==QUIT:
             pygame.quit()
+    
+    if nuvole[-1].x < 150:
+        nuvole.append(nuvolette())
+
+    if tom_y<-20 or tom_y>370:
+        gameover()
 
     immagini()
     aggiorna()
